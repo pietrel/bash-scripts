89dfef6a077c3638c88592c30630917ca635c923
pietrel <piotr.kurek@gmail.com>
2024-08-29 19:30:49 +0200

    Create split-image.sh

---
+++ split-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if $2 is provided
+if [[ -z "$2" ]]; then
+  echo "Usage: $0 <file> <number of slices>"
+  exit 1
+fi
+
+width=$(identify -format "%w" $1)
+
+part_width=$((width / $2))
+
+for i in $(seq 0 $(($2-1))); do
+  magick $1 -crop ${part_width}x+$((i * part_width))+0 part$((i + 1)).png
+done
